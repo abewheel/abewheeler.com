d75838ed38b79cdb26db239c21f8dc5a964e0128
= <abe.wheeler3@gmail.com>
2018-02-09 16:28:43 -0800

    relative paths from root

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	// Set header
-	$('.menu-header').load('../header.html');
-	$('.footer').load('../footer.html');
+	$('.menu-header').load('/header.html');
+	$('.footer').load('/footer.html');
 
 	// Smooth scroll
 	$('a[href^="#"]').on("click", function(e) {
